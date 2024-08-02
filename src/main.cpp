50220a613cae3ec730bf59e871cf585b2192c618
Ruminini <63514002+Ruminini@users.noreply.github.com>
2024-08-01 22:58:37 -0300

    Add: LittleFS for easier website management

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <DHT.h>
+#include <LittleFS.h>
 #include <config.h>
 
 #define RELAYPIN 2
@@ -30,6 +31,9 @@ void handleSet();
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  if (!LittleFS.begin()) {
+  Serial.println("An Error has occurred while mounting LittleFS");
+  }
 
   pinMode(RELAYPIN, OUTPUT);
 
@@ -86,83 +90,24 @@ void regulateTemp() {
   lastUpdate = millis();
   if (forceState != -1) {
     digitalWrite(RELAYPIN, 1 - forceState);
-  } else  if (tem < targetTemp - hysteresis/2) {
+  } else  if (tem < targetTemp - (float)hysteresis/2) {
     digitalWrite(RELAYPIN, 0);
-  } else if (tem > targetTemp + hysteresis/2) {
+  } else if (tem > targetTemp + (float)hysteresis/2) {
     digitalWrite(RELAYPIN, 1);
   }
 }
 
 void handleRoot() {
-  String html = R""""(
-<!DOCTYPE html>
-<html>
-<head>
-	<title>ESP-01 Thermostat</title>
-</head>
-<body>
-	<h1>ESP-01 Thermostat</h1>
-	<p>Temperature: <span id='temperature'></span>&#8451;</p>
-	<p>Humidity: <span id='humidity'></span>%</p>
-	<p>Relay State: <span id='relayState'></span></p>
-	<label for="force">Force</label>
-	<input onclick='toggleForce()' type="checkbox" id="force">
-	<button id="relay" onclick='toggleRelay()'>On</button>
-	<label for="targetTemp">Target Temperature: <span id="targetTempValue">20</span></label>
-	<input type="range" id="targetTemp" min="15" max="30" step="1" value="20" oninput="updateValue('targetTemp')">
-	<label for="hysteresis">Hysteresis: <span id="hysteresisValue">2</span></label>
-	<input type="range" id="hysteresis" min="0" max="10" step="1" value="2" oninput="updateValue('hysteresis')">
-	<label for="updatePeriod">Update Period: <span id="updatePeriodValue">300</span>sec</label>
-	<input type="range" id="updatePeriod" min="15" max="600" step="15" value="300"  oninput="updateValue('updatePeriod')">
-	<script>
-		function toggleForce() {
-			const force = document.getElementById('force').checked;
-			document.getElementById('relay').disabled = !force;
-			document.getElementById('targetTemp').disabled = force;
-			document.getElementById('hysteresis').disabled = force;
-			document.getElementById('updatePeriod').disabled = force;
-			toggleRelay(force ? relay == 'ON' ? 1 : 0 : -1);
-		}
-		let relay = 'OFF';
-		let updateForm = true;
-		function toggleRelay(state = relay == 'ON' ? 0 : 1) {
-			fetch('/set?relay=' + state).then(fetchData);
-		}
-		let debounceTimer;
-		function updateValue(id) {
-				const element = document.getElementById(id);
-				const valueElement = document.getElementById(id + 'Value');
-				valueElement.textContent = element.value;
-				clearTimeout(debounceTimer);
-				debounceTimer = setTimeout(() => {
-					fetch('/set?' + id + '=' + element.value);
-				}, 500);
-		}
-		function fetchData() {
-			fetch('/data').then(response => response.json()).then(data => {
-				relay = data.relay == 0 ? 'OFF' : 'ON';
-				document.getElementById('temperature').innerText = data.temperature;
-				document.getElementById('humidity').innerText = data.humidity;
-				document.getElementById('relayState').innerText = relay;
-				document.getElementById('relay').innerText = relay;
-				const targetTemp = data.targetTemp;
-				const hysteresis = data.hysteresis;
-				const updatePeriod = data.updatePeriod/1000;
-				document.getElementById('targetTemp').value = targetTemp;
-				document.getElementById('hysteresis').value = hysteresis;
-				document.getElementById('updatePeriod').value = updatePeriod;
-				updateValue('targetTemp');
-				updateValue('hysteresis');
-				updateValue('updatePeriod');
-			});
-		}
-		setInterval(fetchData, 5000);
-		fetchData();
-		toggleForce();
-	</script>
-</body>
-</html>
-)"""";
+  File file = LittleFS.open("/index.html", "r"); // Replace "index.html" with your file name
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  String html;
+  while (file.available()) {
+    html += char(file.read());
+  }
+  file.close();
   server.send(200, "text/html", html);
 }
 
@@ -211,5 +156,5 @@ void handleSet() {
   }
   if (error != "") {
     server.send(400, "text/plain", error);
-  } else server.send(200, "text/plain", "OK");
+  } else server.send(200, "text/json", "{success:true}");
 }
