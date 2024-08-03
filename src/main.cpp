0be9a044c173041f7abfe4c940c274befc73d7e8
Ruminini <63514002+Ruminini@users.noreply.github.com>
2024-08-03 15:40:52 -0300

    Feat: Schedule temp
    Now it can receive a schedule single repeats the same every day, weekly allows different weekday and weekend or daily that accepts a custom schedule for each day.
    Also switched to ESPAsyncWebServer and added Json Support trough ArduinoJson

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <DHT.h>
-#include <LittleFS.h>
+#include <ArduinoJson.h>
 #include <config.h>
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
 
 #define RELAYPIN 2
 #define DHTPIN 0
@@ -13,8 +14,9 @@ DHT dht(DHTPIN, DHTTYPE);
 float tem;
 float hum;
 unsigned long int lastRead;
+JsonDocument schedule;
 
-ESP8266WebServer server(80);
+AsyncWebServer server(80);
 
 IPAddress local_IP(192, 168, 1, 11);
 IPAddress gateway(192, 168, 1, 1);
@@ -24,9 +26,16 @@ IPAddress secondaryDNS(8, 8, 4, 4);
 
 void readDht();
 void regulateTemp();
-void handleRoot();
-void handleData();
-void handleSet();
+void handleRoot(AsyncWebServerRequest *request);
+void handleData(AsyncWebServerRequest *request);
+void handleSet(AsyncWebServerRequest *request);
+void handleSchedule(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total);
+bool validateSchedule(JsonArray schedule);
+bool validateTime(JsonArray time);
+int compareTime(int hour1, int minute1, int hour2, int minute2);
+int compareTime(JsonArray time1, JsonArray time2);
+int compareTimeRange(tm * now, JsonObject range);
+void updateTargetTemp();
 
 void setup() {
   Serial.begin(115200);
@@ -34,8 +43,9 @@ void setup() {
   if (!LittleFS.begin()) {
   Serial.println("An Error has occurred while mounting LittleFS");
   }
-
+  configTime(-3 * 3600, 0, "ntp.inti.gob.ar", "pool.ntp.org");
   pinMode(RELAYPIN, OUTPUT);
+  digitalWrite(RELAYPIN, 1);
 
   WiFi.hostname("ESP-Thermostat");
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
@@ -54,9 +64,15 @@ void setup() {
   }
   Serial.println("WiFi connected");
 
-  server.on("/", handleRoot);
-  server.on("/data", handleData);
-  server.on("/set", handleSet);
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/data", HTTP_GET, handleData);
+  server.on("/set", HTTP_POST, handleSet);
+  server.on("/schedule", HTTP_POST, [](AsyncWebServerRequest *request){}, NULL, handleSchedule);
+
+  server.onNotFound([](AsyncWebServerRequest *request){
+    String url = request->url();
+    request->send(LittleFS, url, "text/"+url.substring(url.lastIndexOf('.') + 1));
+  });
 
   server.begin();
   Serial.println("Server started");
@@ -64,7 +80,6 @@ void setup() {
 }
 
 void loop() {
-  server.handleClient();
   readDht();
   regulateTemp();
 }
@@ -88,30 +103,44 @@ int forceState = -1;
 void regulateTemp() {
   if (millis() - lastUpdate < updatePeriod) return;
   lastUpdate = millis();
-  if (forceState != -1) {
+  if (forceState == 0 || forceState == 1) {
     digitalWrite(RELAYPIN, 1 - forceState);
-  } else  if (tem < targetTemp - (float)hysteresis/2) {
-    digitalWrite(RELAYPIN, 0);
-  } else if (tem > targetTemp + (float)hysteresis/2) {
-    digitalWrite(RELAYPIN, 1);
-  }
-}
-
-void handleRoot() {
-  File file = LittleFS.open("/index.html", "r"); // Replace "index.html" with your file name
-  if (!file) {
-    Serial.println("Failed to open file for reading");
     return;
+  } else if (forceState > 1){
+    targetTemp = forceState;
+  } else updateTargetTemp();
+  if (targetTemp == 0 || tem > targetTemp + (float)hysteresis/2) {
+    digitalWrite(RELAYPIN, 1);
+  } else if (tem < targetTemp - (float)hysteresis/2) {
+    digitalWrite(RELAYPIN, 0);
   }
-  String html;
-  while (file.available()) {
-    html += char(file.read());
-  }
-  file.close();
-  server.send(200, "text/html", html);
+  serializeJsonPretty(schedule["7"], Serial);
 }
 
-void handleData() {
+void updateTargetTemp() {
+  time_t now = time(nullptr);
+  struct tm * timeinfo;
+  timeinfo = localtime(&now);
+  JsonArray temps = schedule[String(timeinfo->tm_wday+1)];
+  for (JsonObject item: temps) {
+    int inRange = compareTimeRange(timeinfo, item);
+    Serial.println(inRange);
+    Serial.println((int)item["temperature"]);
+    if (inRange == 0) {
+      targetTemp = item["temperature"];
+      return;
+    } else if (inRange == -1) {
+      break;
+    }
+  }
+  targetTemp = 0;
+}
+
+void handleRoot(AsyncWebServerRequest *request) {
+  request->send(LittleFS, "/index.html", "text/html");
+}
+
+void handleData(AsyncWebServerRequest *request) {
   int relay = forceState!= -1 ? forceState : 1 - digitalRead(RELAYPIN);
   String json = "{\"temperature\":" + String(tem) +
   " , \"humidity\":" + String(hum) +
@@ -120,41 +149,138 @@ void handleData() {
   " , \"hysteresis\":" + String(hysteresis) +
   " , \"updatePeriod\":" + String(updatePeriod) +
   "}";
-  Serial.println(json);
-  server.send(200, "application/json", json);
+  request->send(200, "application/json", json);
 }
 
-void handleSet() {
+void handleSet(AsyncWebServerRequest *request) {
   String error;
-  if (server.hasArg("relay")) {
-    int relay = server.arg("relay").toInt();
-    if (relay >= -1 && relay <= 1) {
-      forceState = relay;
-      Serial.println("Relay state changed to " + String(relay));
-    } else error += "Relay must be -1(Auto), 0(Off) or 1(On)\n";
+  String ok;
+  if (request->hasArg("force")) {
+    int nForce = request->arg("force").toInt();
+    if ((nForce >= -1 && nForce <= 1) || (nForce >= 15 && nForce <= 30)) {
+      forceState = nForce;
+      ok += "Force state changed to " + String(nForce) + "\n";
+    } else error += "Force must be -1(Auto), 0(Off), 1(On) or a temp between 15 and 30\n";
   }
-  if (server.hasArg("targetTemp")) {
-    int nTargetTemp = server.arg("targetTemp").toInt();
-    if (targetTemp >= 15 && targetTemp <= 30) {
-      targetTemp = nTargetTemp;
-      Serial.println("Target temperature changed to " + String(targetTemp));
-    } else error += "Target temperature must be between 15 and 30\n";
-  }
-  if (server.hasArg("hysteresis")) {
-    int nHysteresis = server.arg("hysteresis").toInt();
-    if (hysteresis >= 0 && hysteresis <= 10) {
+  if (request->hasArg("hysteresis")) {
+    int nHysteresis = request->arg("hysteresis").toInt();
+    if (nHysteresis >= 0 && nHysteresis <= 10) {
       hysteresis = nHysteresis;
-      Serial.println("Hysteresis changed to " + String(hysteresis));
+      ok += "Hysteresis changed to " + String(hysteresis) + "\n";
     } else error += "Hysteresis must be between 0 and 10\n";
   }
-  if (server.hasArg("updatePeriod")) {
-    int nUpdatePeriod = server.arg("updatePeriod").toInt();
-    if (updatePeriod >= 1000 && updatePeriod <= 600000) {
+  if (request->hasArg("updatePeriod")) {
+    int nUpdatePeriod = request->arg("updatePeriod").toInt();
+    if (nUpdatePeriod >= 1000 && nUpdatePeriod <= 600000) {
       updatePeriod = nUpdatePeriod;
-      Serial.println("Update period changed to " + String(updatePeriod));
+      ok += "Update period changed to " + String(updatePeriod) + "\n";
     } else error += "Update period must be between 1000 and 600000\n";
   }
-  if (error != "") {
-    server.send(400, "text/plain", error);
-  } else server.send(200, "text/json", "{success:true}");
+
+  if (error == "") {
+    request->send(200, "text/json", "{\"success\":true, \"message\":\"" + ok + "\"}");
+  } else request->send(400, "text/plain", error);
+}
+
+String jsonData;
+void handleSchedule(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+  for (size_t i = 0; i < len; i++) {
+    jsonData += (char)data[i];
+  }
+  if (index + len != total) return;
+  JsonDocument json;
+  DeserializationError error = deserializeJson(json, jsonData);
+  jsonData = "";
+  if (error) {
+    request->send(400, "text/json", "{\"success\":false, \"error\":\"Invalid JSON\"}");
+    return;
+  }
+  // Example: {"schedule": [{"start_time": [0, 0], "end_time": [8, 0], "temperature": 20},{"start_time": [8, 0], "end_time": [16, 0], "temperature": 25},{"start_time": [16, 0], "end_time": [23,59], "temperature": 20}]}
+  String path = request->url();
+  if (path == "/schedule/single") {
+    if (!validateSchedule(json["schedule"])) {
+      request->send(400, "text/json", "{\"success\":false, \"error\":\"Invalid schedule\"}");
+      return;
+    }
+    schedule.clear();
+    for (int day = 1; day <= 7; day++) {
+      schedule[(String)day] = json["schedule"];
+    }
+    request->send(200, "text/json", "{\"success\":true, \"schedule\": \"single\"}");
+  }
+  // Example: {"weekdays": [{"start_time": [0, 0], "end_time": [8, 0], "temperature": 20},{"start_time": [8, 0], "end_time": [16, 0], "temperature": 25},{"start_time": [16, 0], "end_time": [23, 59], "temperature": 20}], "weekends": [{"start_time": [0, 0], "end_time": [10, 0], "temperature": 20},{"start_time": [10, 0], "end_time": [16, 0], "temperature": 18},{"start_time": [16, 0], "end_time": [23, 59], "temperature": 22}]}
+  if (path == "/schedule/weekly") {
+    if ( !json.containsKey("weekdays") ||
+         !json.containsKey("weekends") ||
+         !validateSchedule(json["weekdays"]) ||
+         !validateSchedule(json["weekends"])) {
+      request->send(400, "text/json", "{\"success\":false, \"error\":\"Invalid schedule\"}");
+      return;
+    }
+    schedule.clear();
+    schedule["1"] = json["weekends"];
+    schedule["7"] = json["weekends"];
+    for (int day = 2; day <= 6; day++) {
+      schedule[(String)day] = json["weekdays"];
+    }
+    request->send(200, "text/json", "{\"success\":true}");
+  }
+  // Example: {"1": [{"start_time": [0, 0], "end_time": [8, 0], "temperature": 20}],"2":[{"start_time": [8, 0], "end_time": [16, 0], "temperature": 25}],"3":[{"start_time": [16, 0], "end_time": [23, 59], "temperature": 20}],"4": [{"start_time": [0, 0], "end_time": [8, 0], "temperature": 20}],"5":[{"start_time": [8, 0], "end_time": [16, 0], "temperature": 25}],"6":[{"start_time": [16, 0], "end_time": [23, 59], "temperature": 20}],"7": [{"start_time": [0, 0], "end_time": [23, 59], "temperature": 20}]}
+  if (path == "/schedule/daily") {
+    for (int day = 1; day <= 7; day++) {
+      if ( !json.containsKey((String)day) || !validateSchedule(json[(String)day])) {
+        request->send(400, "text/json", "{\"success\":false, \"error\":\"Invalid schedule\"}");
+        return;
+      }
+    }
+    schedule.clear();
+    for (int day = 1; day <= 7; day++) {
+      schedule[(String)day] = json[(String)day];
+    }
+    request->send(200, "text/json", "{\"success\":true}");
+  }
+}
+
+bool validateSchedule(JsonArray schedule) {
+  JsonObject prevItem;
+  for (JsonObject item: schedule) {
+    if (!item["temperature"].is<float>()) return false;
+    if (item["temperature"] < 15 || item["temperature"] > 30) return false;
+    if (!validateTime(item["start_time"]) || !validateTime(item["end_time"])) return false;
+    if (compareTime(item["start_time"], item["end_time"]) != -1) return false;
+    if (prevItem) {
+      if (compareTime(prevItem["end_time"], item["start_time"]) == 1) return false;
+    }
+    prevItem = item;
+  }
+  return true;
+}
+
+bool validateTime(JsonArray time) {
+  if (time.size() != 2) return false;
+  if (!time[0].is<int>() || !time[1].is<int>()) return false;
+  if (time[0] < 0 || time[0] > 24) return false;
+  if (time[1] < 0 || time[1] > 59) return false;
+  if (time[0] == 24 && time[1] != 0) return false;
+  return true;
+}
+
+int compareTime(int hour1, int minute1, int hour2, int minute2) {
+  if (hour1 < hour2) return -1;
+  if (hour1 > hour2) return 1;
+  if (minute1 < minute2) return -1;
+  if (minute1 > minute2) return 1;
+  return 0;
+}
+
+int compareTime(JsonArray time1, JsonArray time2) {
+  return compareTime(time1[0], time1[1], time2[0], time2[1]);
+}
+
+int compareTimeRange(tm * now, JsonObject range) {
+  JsonArray start = range["start_time"];
+  JsonArray end = range["end_time"];
+  if (compareTime(now->tm_hour, now->tm_min, end[0], end[1]) == 1) return 1;
+  if (compareTime(now->tm_hour, now->tm_min, start[0], start[1]) == -1) return -1;
+  return 0;
 }
